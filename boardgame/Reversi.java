1ebfaccb8db08ecc508e32caf6b3bf1dd1d266ab
Oshello404 <84498337+Oshello404@users.noreply.github.com>
2022-05-23 18:31:47 +0800

    Update Reversi.java
    Utilizing JOptionPane to play a game of Reversi(Othello) and TicTacToe. The class BoardGame and TurnBasedGame is given as the framework of the program.

--- boardgame/Reversi.java
+++ boardgame/Reversi.java
@@ -1,27 +1,3 @@
-/**
- * CSCI1130 Java Assignment 6 BoardGame Reversi
- * Aim: Practise subclassing, method overriding
- *      Learn from other subclass examples
- * 
- * I declare that the assignment here submitted is original
- * except for source material explicitly acknowledged,
- * and that the same or closely related material has not been
- * previously submitted for another course.
- * I also acknowledge that I am aware of University policy and
- * regulations on honesty in academic work, and of the disciplinary
- * guidelines and procedures applicable to breaches of such
- * policy and regulations, as contained in the website.
- *
- * University Guideline on Academic Honesty:
- *   http://www.cuhk.edu.hk/policy/academichonesty
- * Faculty of Engineering Guidelines to Academic Honesty:
- *   https://www.erg.cuhk.edu.hk/erg/AcademicHonesty
- *
- * Student Name:  Hans Nathanael Junoes
- * Student ID  :  1155174034
- * Date        :  13/12/2021
- */
-
 package boardgame;
 
 import java.awt.Color;
